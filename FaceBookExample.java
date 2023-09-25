c733d13987a7b6a26d32efead6bc6c94cb1dce75
MuhayoMiniyarova <muhayominiyarova@gmail.com>
2023-09-25 12:21:38 -0400

    first commit

---
+++ FaceBookExample.java
@@ -0,0 +1,26 @@
+package class02;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class FaceBookExample {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com");
+
+        driver.manage().window().maximize();
+        WebElement email=driver.findElement(By.id("email"));
+        email.sendKeys("muhayom@gmail.com");
+
+        WebElement password=driver.findElement(By.name("pass"));
+        password.sendKeys("abracadabra123");
+
+        WebElement loginBtn=driver.findElement(By.name("login"));
+        loginBtn.click();
+
+    }
+}
